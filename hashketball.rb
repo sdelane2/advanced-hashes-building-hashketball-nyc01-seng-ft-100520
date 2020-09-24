e2febfc3fc315f6eea5218206e9f4b26d64b24c8
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-24 18:02:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,7 @@
 # Write your code here!
 def game_hash(hash)
 
-hash = {
-  :home => {:team_name => "Brooklyn Nets",
-  :colors => "Black, White",
-  :players => [{:player_name => "Alan Anderson", 
+hash = {:home => {:team_name => "Brooklyn Nets", :colors => "Black, White", :players => [{:player_name => "Alan Anderson", 
     :number => 0,
     :shoe => 16,
     :points => 22,
@@ -13,7 +10,7 @@ hash = {
     :steals => 3,
     :blocks => 1,
     :slam_dunks => 1}, {
-      player_name => "Reggie Evans", 
+    :player_name => "Reggie Evans", 
     :number => 30,
     :shoe => 14,
     :points => 12,
@@ -22,10 +19,58 @@ hash = {
     :steals => 12,
     :blocks => 12,
     :slam_dunks => 12}, {
+    :player_name => "Brook Lopez",
+    :number => 11,
+    :shoe => 17,
+    :points => 17,
+    :rebounds => 19,
+    :assists => 10,
+    :steals => 3,
+    :blocks => 1,
+    :slam_dunks => 15},
+    {
+    :player_name => "Mason Plumlee",
+    :number => 1,
+    :shoe => 19,
+    :points => 26,
+    :rebounds => 11,
+    :assists => 6,
+    :steals => 3,
+    :blocks => 8,
+    :slam_dunks => 5},
+    {
+    :player_name => "Jason Terry",
+    :number => 31,
+    :shoe => 15,
+    :points => 19,
+    :rebounds => 2,
+    :assists => 2,
+    :steals => 4,
+    :blocks => 11,
+    :slam_dunks => 1}]},
+  :away => {:team_name => "Charlotte Hornets", :colors => "Turquoise, Purple", :players => [{
+    :player_name => "Jeff Adrien",
+    :number => 4,
+    :shoe => 18,
+    :points => 10,
+    :rebounds => 1,
+    :assists => 1,
+    :steals => 2,
+    :blocks => 7,
+    :slam_dunks => 2},
+    {
+    :player_name => "Bismack Biyombo",
+    :number => 0,
+    :shoe => 16,
+    :points => 12,
+    :rebounds => 4,
+    :assists => 7,
+    :steals => 22,
+    :blocks => 15,
+    :slam_dunks => 10},
+    {
     :player_name =>
-    }]
-    
-  },
-  :away => {}
+    }
+    ]}
 }
 end
